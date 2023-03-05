146252827c3b712a91f0be8bc7d0c5a6833b9557
Joakim Tall <joakim.tall@bontouch.com>
2023-03-05 10:58:52 +0100

    Nothing works

--- app/src/main/java/com/example/dialogbugs/MainActivity.kt
+++ app/src/main/java/com/example/dialogbugs/MainActivity.kt
@@ -1,27 +1,48 @@
 package com.example.dialogbugs
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.DialogWindowProvider
+import androidx.core.view.WindowCompat
 import com.example.dialogbugs.ui.theme.DialogBugsTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             DialogBugsTheme {
-                Screen()
+                println("Root window: ${findWindow(LocalView.current)}")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Screen()
+                }
             }
         }
     }
@@ -30,23 +51,75 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun Screen() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.hsl(200f, 0.5f, 0.5f))) {
-        Dialog(onDismissRequest = {}, properties = DialogProperties(usePlatformDefaultWidth = false)) {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Color.hsl(150f, 0.5f, 0.5f))) {
-                Text(text = "Hello Dialog!")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding(),
+    ) {
+        var showDialog by remember { mutableStateOf(true) }
+        Button(onClick = { showDialog = true }) {
+            Text(text = "Show dialog")
+        }
+        if (showDialog) {
+            Dialog(onDismissRequest = {}, properties = DialogProperties(usePlatformDefaultWidth = false)) {
+                var dialogColor by remember {
+                    mutableStateOf(
+                        Color.hsl(Math.random().toFloat() * 255, 0.5f, 0.5f)
+                    )
+                }
+                LaunchedEffect(Unit) {
+                    while (isActive) {
+                        dialogColor = Color.hsl(Math.random().toFloat() * 255, 0.5f, 0.5f)
+                        delay(10000)
+                    }
+                }
+                println("Dialog window: ${findWindow(LocalView.current)}")
+                findWindow(LocalView.current)?.let { window ->
+                    WindowCompat.setDecorFitsSystemWindows(window, false)
+                    window.apply {
+                        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                        statusBarColor = Color.Transparent.toArgb()
+                        navigationBarColor = Color.Transparent.toArgb()
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            isNavigationBarContrastEnforced = false
+                        }
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(dialogColor)
+                ) {
+                }
+                Button(onClick = { showDialog = false }) {
+                    Text(text = "Close dialog")
+                }
             }
         }
     }
 }
 
+@Composable
+private fun findWindow(view: View): Window? {
+    val parent = view.parent
+    return if (parent is DialogWindowProvider)
+        parent.window
+    else view.context.findWindow()
+}
+
+private tailrec fun Context.findWindow(): Window? =
+    when (this) {
+        is Activity -> window
+        is ContextWrapper -> baseContext.findWindow()
+        else -> null
+    }
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DialogBugsTheme {
-        Screen()
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Screen()
+        }
     }
 }
--- app/src/main/java/com/example/dialogbugs/ui/theme/Theme.kt
+++ app/src/main/java/com/example/dialogbugs/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -22,6 +23,7 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
+    background = Color.hsl(200f, 0.5f, 0.5f),
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
@@ -44,18 +46,18 @@ fun DialogBugsTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = LightColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            (view.context as Activity).window.apply {
+                statusBarColor = Color.Transparent.toArgb()
+                navigationBarColor = Color.Transparent.toArgb()
+                //navigationBarColor = colorScheme.primary.toArgb()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    isNavigationBarContrastEnforced = false
+                }
+            }
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
         }
     }
